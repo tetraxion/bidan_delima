a89540c987c6769c7767cc0c9995474143ef7107
Tetraxion <lutfid955@gmail.com>
2023-11-21 01:23:52 +0700

    fist commit

--- lib/home.dart
+++ lib/home.dart
@@ -2,73 +2,161 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(120.0),
-        child: AppBar(
-          backgroundColor: Color.fromARGB(255, 232, 194, 194),
-          centerTitle: true,
-          leading: Center(
-            child: Image.asset(
-              "assets/images/logo.png",
-              width: 200.0,
-              height: 200.0,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color.fromARGB(255, 232, 194, 194),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("assets/images/loadingbg.png"),
+          fit: BoxFit.cover,
+        )),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            Container(
+              height: 190,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 232, 194, 194),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(),
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      width: 50.0,
+                      height: 60.0,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 232, 194, 194),
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'TPMB TMM DJAMINI',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Gemol ID No 38 Wiyung Surabaya',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 232, 194, 194),
+                    ),
+                    child: const Icon(
+                      Icons.login_outlined,
+                      size: 20.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'TPMB TMM DJAMINI',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Gemol ID No 38 Wiyung Surabaya',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          actions: [
-            Center(
-              child: IconButton(
-                icon: Icon(
-                  Icons.login_outlined,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 130,
+              bottom: 0,
+              child: SingleChildScrollView(
+                  child: Column(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x19000000),
+                          blurRadius: 24,
+                          offset: Offset(0, 11),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          child: const Icon(
+                            Icons.date_range_outlined,
+                            size: 24.0,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          child: const Icon(
+                            Icons.date_range_outlined,
+                            size: 24.0,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          child: const Icon(
+                            Icons.date_range_outlined,
+                            size: 24.0,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          child: const Icon(
+                            Icons.date_range_outlined,
+                            size: 24.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
             ),
           ],
-          toolbarHeight: 120.0,
         ),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            height: screenHeight, 
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/loadingbg.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
